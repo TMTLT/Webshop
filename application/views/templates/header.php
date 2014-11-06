828c0dec32bdb4211cc0d543869e7f3a0f69b9d7
Miesvanderlippe <miesvanderlippe@gmail.com>
2014-11-06 15:01:39 +0100

    Removed slider from header
    Now is part of content

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -187,20 +187,3 @@
 		 </div>
 	  </header>
    </div>
-   <!--Banner Block-->
-   <section class="banner-wrapper">
-	  <div class="banner-block container">
-		 <div class="flexslider">
-			<ul class="slides">
-			   <li><img title="Banner" alt="Banner" src="images/banner1.jpg" /></li>
-			   <li><img title="Banner" alt="Banner" src="images/banner2.jpg" /></li>
-			   <li><img title="Banner" alt="Banner" src="images/banner3.jpg" /></li>
-			   <li><img title="Banner" alt="Banner" src="images/banner4.jpg" /></li>
-			</ul>
-		 </div>
-		 <ul class="banner-add">
-			<li class="add1"><a href="#" title=""><img title="add1" alt="add1" src="images/static1.jpg" /></a></li>
-			<li class="add2"><a href="#" title=""><img title="add2" alt="add2" src="images/static2.jpg" /></a></li>
-		 </ul>
-	  </div>
-   </section>
