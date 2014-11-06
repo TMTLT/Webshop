8e3e257d57066d65c007b9d0441cb4a45e1d43c7
Miesvanderlippe <miesvanderlippe@gmail.com>
2014-11-06 16:30:37 +0100

    Big derp

--- application/models/account_model.php
+++ application/models/account_model.php
@@ -7,7 +7,7 @@ TODO :
 
 require_once(APPPATH . "application/classes/crypt.php");
 
-class News_model extends CI_Model {
+class Account_model extends CI_Model {
 
 	public function __construct()
 	{
