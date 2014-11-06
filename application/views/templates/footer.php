b6bfa51c38e35e72846c5546ee7cd8e98faa47f0
Miesvanderlippe <miesvanderlippe@gmail.com>
2014-11-06 16:09:10 +0100

    Fixed up footer links

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -3,8 +3,8 @@
   <footer class="container">
 	<div class="link-block">
 	  <ul>
-		<li class="link-title"><a href="about_us.html" title="ABOUT US">ABOUT US</a></li>
-		<li><a href="about_us.html" title="About Us">About Us</a></li>
+		<li class="link-title"><a href="/about_us.html" title="ABOUT US">ABOUT US</a></li>
+		<li><a href="/about_us.html" title="About Us">About Us</a></li>
 		<li><a href="#" title="Customer Service">Customer Service</a></li>
 		<li><a href="#" title="Privacy Policy">Privacy Policy</a></li>
 	  </ul>
@@ -12,7 +12,7 @@
 		<li class="link-title"><a href="#" title="CUSTOMER SERVICES">CUSTOMER SERVICES</a></li>
 		<li><a href="#" title="Shipping & Returns">Shipping & Returns</a></li>
 		<li><a href="#" title="Secure Shopping">Secure Shopping</a></li>
-		<li><a href="contact_us.html" title="Contact Us">Contact Us</a></li>
+		<li><a href="/contact_us.html" title="Contact Us">Contact Us</a></li>
 	  </ul>
 	  <ul>
 		<li class="link-title"><a href="#" title="TERMS & CONDITIONS">TERMS & CONDITIONS</a></li>
@@ -25,29 +25,29 @@
 		<li class="aboutus-block">Lorem ipsum dolor sit amet,
 		  consectetur adipiscing elit. Vivamus sit
 		  amet ligula lectus, a mollis diam. Nulla
-		  porttitor pulvinar elit... <a href="about_us.html" title="read more">read more</a> </li>
+		  porttitor pulvinar elit... <a href="/about_us.html" title="read more">read more</a> </li>
 	  </ul>
 	  <ul class="stay-connected-blcok">
 		<li class="link-title"><a href="#" title="STAY CONNECTED...">STAY CONNECTED...</a></li>
 		<li>
 			 <ul class="social-links">
-				<li><a data-tooltip="Like us on facebook" href="#"><img alt="facebook" src="images/facebook.png"></a></li>
-				<li><a data-tooltip="Subscribe to RSS feed" href="#"><img alt="RSS" src="images/rss.png"></a></li>
-				<li><a data-tooltip="Follow us on twitter" href="#"><img alt="twitter" src="images/twitter.png"></a></li>
-				<li><a data-tooltip="Follow us on Dribbble" href="#"><img alt="dribbble" src="images/dribbble.png"></a></li>
-				<li><a data-tooltip="Follow us on Youtube" href="#"><img alt="youtube" src="images/youtube.png"></a></li>
+				<li><a data-tooltip="Like us on facebook" href="#"><img alt="facebook" src="/images/facebook.png"></a></li>
+				<li><a data-tooltip="Subscribe to RSS feed" href="#"><img alt="RSS" src="/images/rss.png"></a></li>
+				<li><a data-tooltip="Follow us on twitter" href="#"><img alt="twitter" src="/images/twitter.png"></a></li>
+				<li><a data-tooltip="Follow us on Dribbble" href="#"><img alt="dribbble" src="/images/dribbble.png"></a></li>
+				<li><a data-tooltip="Follow us on Youtube" href="#"><img alt="youtube" src="/images/youtube.png"></a></li>
 				<li><a data-tooltip="Follow us on skype" href="#"><img alt="skype" src="images/skype.png"></a></li>
 			 </ul>
-			 <div class="payment-block"><img src="images/payment.png" alt="payment"></div>
+			 <div class="payment-block"><img src="/images/payment.png" alt="payment"></div>
 		</li>
 	  </ul>
 	</div>
 	<div class="footer-bottom-block">
 	  <ul class="bottom-links">
-		<li><a href="index-2.html" title="Home">HOME</a></li>
+		<li><a href="/index-2.html" title="Home">HOME</a></li>
 		<li><a href="#" title="Pages">PAGES</a></li>
-		<li><a href="about_us.html" title="About">ABOUT</a></li>
-		<li><a href="contact_us.html" title="Contact">CONTACT</a></li>
+		<li><a href="/about_us.html" title="About">ABOUT</a></li>
+		<li><a href="/contact_us.html" title="Contact">CONTACT</a></li>
 	  </ul>
 	  <p class="copyright-block">© 2014 MO's Webshop, All Rights Reserved.</p>
 	</div>
