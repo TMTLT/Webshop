da638ae25b7ccd0b251c9c3128e4e0375de1ef20
Owain van Brakel <owain.vanbrakel@gmail.com>
2014-11-13 21:13:57 +0100

    Remove annoying CI error && edit database file for MAMP

--- application/config/database.php
+++ application/config/database.php
@@ -50,7 +50,7 @@ $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
 $db['default']['username'] = 'root';
-$db['default']['password'] = 'usbw';
+$db['default']['password'] = 'root';
 $db['default']['database'] = 'dbtmtl_06';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
