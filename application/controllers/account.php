20e453ba6c653e6b8038181ee8325dd2e06f7eaa
Owain van Brakel <owain.vanbrakel@gmail.com>
2014-11-13 21:18:05 +0100

    Cleanup code

--- application/controllers/account.php
+++ application/controllers/account.php
@@ -3,69 +3,69 @@
     /**
      * Class Account
      */
-    class Account extends MY_Controller{
+    class Account extends MY_Controller {
 
         /**
          *
          */
         public function __construct() {
-		parent::__construct();
-		$this->load->helper('url');
-	}
+            parent::__construct();
+            $this->load->helper('url');
+        }
 
         /**
          *
          */
         public function index() {
-		redirect('/account/login');
-	}
+            redirect('/account/login');
+        }
 
         /**
          *
          */
         public function login() {
-		$data = $this->data;
-		$data['title'] = 'Login';
+            $data          = $this->data;
+            $data['title'] = 'Login';
 
-		$this->load->model('Account_model');
+            $this->load->model('Account_model');
 
-		print_r($this->Account_model->debug());
+            print_r($this->Account_model->debug());
 
-		$this->load->template('account/login', $data);
-	}
+            $this->load->template('account/login', $data);
+        }
 
         /**
          *
          */
         public function register() {
-		$data = $this->data;
-		$data['title'] = 'Registreren';
+            $data          = $this->data;
+            $data['title'] = 'Registreren';
 
-		$this->load->model('Account_model');
+            $this->load->model('Account_model');
 
-		$this->load->template('account/create', $data);
-	}
+            $this->load->template('account/create', $data);
+        }
 
         /**
          *
          */
         public function create() {
-		$data = $this->data;
-		$data['title'] = 'Maak een account';
+            $data          = $this->data;
+            $data['title'] = 'Maak een account';
 
-		$this->load->helper('form');
-		$this->load->template('account/create', $data);
-	}
+            $this->load->helper('form');
+            $this->load->template('account/create', $data);
+        }
 
         /**
          *
          */
         public function createSubmit() {
-		$data = $this->data;
-		$data['title'] = 'Maak een account';
+            $data          = $this->data;
+            $data['title'] = 'Maak een account';
 
-		$this->load->model('Account_model');
-		$this->Account_model->registerSubmit();
-		//$this->load->template('account/create', $data);
-	}
-}
+            $this->load->model('Account_model');
+            $this->Account_model->registerSubmit();
+            //$this->load->template('account/create', $data);
+        }
+    }
--- application/models/account_model.php
+++ application/models/account_model.php
@@ -1,11 +1,6 @@
 <?php
-/*
-TODO :
-- Everything
 
-*/
-
-require_once(APPPATH . "classes/crypt.php");
+    require_once(APPPATH . "classes/crypt.php");
 
     /**
      * Class Account_model
@@ -15,84 +10,83 @@ require_once(APPPATH . "classes/crypt.php");
         /**
          *
          */
-        public function __construct()
-	{
-		$this->load->database();
-	}
+        public function __construct() {
+            $this->load->database();
+        }
 
         /**
          * @param $username
          * @param $password
          *
          * @return bool
-         */public function login($username, $password) {
+         */
+        public function login($username, $password) {
 
-		return false;
-	}
+            return false;
+        }
 
         /**
          * @return bool
          */
         public function register() {
 
-		return false;
-	}
+            return false;
+        }
 
         /**
          * @return bool
          */
         public function activate() {
 
-		return false;
-	}
+            return false;
+        }
 
         /**
          * @return bool
          */
         public function resetpassword() {
 
-		return false;
-	}
+            return false;
+        }
 
         /**
          * @return array
          */
         public function debug() {
 
-		$salt 	 = Crypt::GetRandomSalt();
-		$hashed  = Crypt::HashPassword('test', $salt);
-		$values  = array(
-			'salt' => $salt,
-			'hashed' => $hashed,
-			'validate' => Crypt::ValidatePassword('test', $hashed)
-		);
+            $salt   = Crypt::GetRandomSalt();
+            $hashed = Crypt::HashPassword('test', $salt);
+            $values = array(
+                'salt'     => $salt,
+                'hashed'   => $hashed,
+                'validate' => Crypt::ValidatePassword('test', $hashed)
+            );
 
-		return $values;
-	}
+            return $values;
+        }
 
         /**
          *
          */
-        public function registerSubmit()
-	{
-		$firstname = $this->input->post('firstname');
-		$affix = $this->input->post('affix');
-		$lastname = $this->input->post('lastname');
-		$zip = $this->input->post('zip_code');
-		$number = $this->input->post('house_number');
-		$email = $this->input->post('email');
-		$password = $this->input->post('password');
-		$password2 = $this->input->post('password2');
-		$salt = Crypt::GetRandomSalt();
-		echo "Voornaam: " . $firstname . "<br />";
-		echo "Tussenvoegsel: " . $affix . "<br />";
-		echo "Achternaam: " . $lastname . "<br />";
-		echo "Postcode: " . $zip . "<br />";
-		echo "Huisnummer: " . $number . "<br />";
-		echo "Email: " . $email . "<br />";
-		echo "Wachtwoord: " . $password . "<br />";
-		echo "Wachtwoord2: " . $password2 . "<br />";
-		echo $salt . "<br />";
-		Crypt::HashPassword($password, $salt);
-	}
-}
+        public function registerSubmit() {
+            $firstname = $this->input->post('firstname');
+            $affix     = $this->input->post('affix');
+            $lastname  = $this->input->post('lastname');
+            $zip       = $this->input->post('zip_code');
+            $number    = $this->input->post('house_number');
+            $email     = $this->input->post('email');
+            $password  = $this->input->post('password');
+            $password2 = $this->input->post('password2');
+            $salt      = Crypt::GetRandomSalt();
+            echo "Voornaam: " . $firstname . "<br />";
+            echo "Tussenvoegsel: " . $affix . "<br />";
+            echo "Achternaam: " . $lastname . "<br />";
+            echo "Postcode: " . $zip . "<br />";
+            echo "Huisnummer: " . $number . "<br />";
+            echo "Email: " . $email . "<br />";
+            echo "Wachtwoord: " . $password . "<br />";
+            echo "Wachtwoord2: " . $password2 . "<br />";
+            echo $salt . "<br />";
+            Crypt::HashPassword($password, $salt);
+        }
+    }
