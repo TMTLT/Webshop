e174f4da330e97995660001eb29eac49a54892f4
miesvanderlippe <miesvanderlippe@gmail.com>
2014-12-04 15:54:32 +0100

    Removed elements we won't use

--- application/views/store/checkout.php
+++ application/views/store/checkout.php
@@ -22,94 +22,15 @@
 					<div class="box-content"><p>Enter your coupon code if you have one.</p></div>
 					<button class="colors-btn" title="Apply Coupon">Apply Coupon</button>
 				</div>
-				<div class="cart-box">
-					<div class="box-title">Estimate Shipping and Tax</div>
-					<div class="box-content">
-						<p>Enter your destnation to get a shipping estimate</p>
-						<ul>
-							<li>
-								<label>Country<em>*</em></label>
-								<select><option>United States</option></select>
-							</li>
-							<li>
-								<label>State/Province</label>
-								<select><option>Please select region, state or province</option></select>
-							</li>
-							<li>
-								<label>Zip/Postal Code</label>
-								<input type="text" />
-							</li>
-						</ul>
-					</div>
-					<button class="colors-btn" title="Get a Quote">Get a Quote</button>
-				</div>
 			</div>
 			<div class="shopping-cart-totals">
 				<div class="subtotal-row"><div class="left">Subtotal</div><div class="right">$1,000,00</div></div>
 				<div class="grand-row"><div class="left">Grand  Total</div><div class="right">$1,000.00</div></div>
 				<ul class="checkout-types">
 					<li><button class="colors-btn" title="Proceed to Checkout">Proceed to Checkout</button></li>
-					<li><a href="#" title="Checkout With Multiple Addresses">Checkout With Multiple Addresses</a></li>
 				</ul>
 			</div>
-			<div class="generic-product-list">
-				<h4>Based on your selection, you may be interested in the following items:</h4>
-				<ul>
-					<li>
-						<div class="left-img"><img src="/images/tshir_img.png" title="" alt="" /></div>
-						<div class="right-content">
-							<div class="pro-name">White Round Neck <br>T-Shirt</div>
-							<div class="pro-price">$600.00</div>
-							<div class="add-cart-row"><button class="colors-btn" title="Add to Cart">Add to Cart</button></div>
-							<a href="#" class="add-wishlist" title="Add to Wishlist">Add to Wishlist</a>
-							<a href="#" class="add-compare" title="Add to Compare">Add to Compare</a>
-						</div>
-					</li>
-					<li>
-						<div class="left-img"><img src="/images/tshir_img.png" title="" alt="" /></div>
-						<div class="right-content">
-							<div class="pro-name">White Round Neck <br>T-Shirt</div>
-							<div class="pro-price">$600.00</div>
-							<div class="add-cart-row"><button class="colors-btn" title="Add to Cart">Add to Cart</button></div>
-							<a href="#" class="add-wishlist" title="Add to Wishlist">Add to Wishlist</a>
-							<a href="#" class="add-compare" title="Add to Compare">Add to Compare</a>
-						</div>
-					</li>
-					<li>
-						<div class="left-img"><img src="/images/tshir_img.png" title="" alt="" /></div>
-						<div class="right-content">
-							<div class="pro-name">White Round Neck <br>T-Shirt</div>
-							<div class="pro-price">$600.00</div>
-							<div class="add-cart-row"><button class="colors-btn" title="Add to Cart">Add to Cart</button></div>
-							<a href="#" class="add-wishlist" title="Add to Wishlist">Add to Wishlist</a>
-							<a href="#" class="add-compare" title="Add to Compare">Add to Compare</a>
-						</div>
-					</li>
-					<li>
-						<div class="left-img"><img src="/images/tshir_img.png" title="" alt="" /></div>
-						<div class="right-content">
-							<div class="pro-name">White Round Neck <br>T-Shirt</div>
-							<div class="pro-price">$600.00</div>
-							<div class="add-cart-row"><button class="colors-btn" title="Add to Cart">Add to Cart</button></div>
-							<a href="#" class="add-wishlist" title="Add to Wishlist">Add to Wishlist</a>
-							<a href="#" class="add-compare" title="Add to Compare">Add to Compare</a>
-						</div>
-					</li>
-				</ul>
-			</div>
-		</div>
 		<div class="clearfix"></div>
-		<div class="news-letter-container">
-			<div class="free-shipping-block">
-				<h1>ENJOY FREE SHIPPING</h1>
-				<p>on all orders as our holiday gift for you!</p>
-			</div>
-			<div class="news-letter-block">
-				<h2>SIGN UP FOR OUR NEWSLETTER</h2>
-				<input type="text" value="Enter email address" title="" />
-				<input class="submit-btn" type="submit" value="Submit" title="Submit" />
-			</div>
-		</div>	
 	</div>
 </section>
 </div>
