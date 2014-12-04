b134da84e670f541f1cf26ed14b2813cfca1e134
miesvanderlippe <miesvanderlippe@gmail.com>
2014-12-04 16:23:41 +0100

    Added working upload to test controller

---
+++ application/views/test/upload.php
@@ -0,0 +1,71 @@
+<!--Content Block-->
+<section class="content-wrapper">
+	<div class="content-container container">
+		<div class="col-main-left">
+			<h1 class="page-title">FAQ</h1>
+			<div class="block-01">
+				<?=var_dump($testdata)?>
+
+
+				<?php echo form_open_multipart('test/upload');?>
+
+					<input type="file" name="userfile" size="20" />
+
+					<br /><br />
+
+					<input type="submit" value="upload" />
+				</form>
+			</div>
+		</div>
+		<aside class="right-sidebar">
+			<div class="block widget-searchwidget">
+				<input type="text" value="Type to search..." />
+			</div>
+			<div class="block widget-img">
+				<a href="#" title=""><img src="/images/Owain.jpg"></a>
+				<a href="#" title=""><img src="/images/Mies.jpg"></a>
+				<a href="#" title=""><img src="/images/Zack.jpg"></a>
+				<a href="http://github.com/tmtlt/Webshop" title=""><img src="/images/Github.jpg"></a>
+			</div>
+			<div class="block listing-block">
+				<p>
+					<strong>Ons team</strong>
+					Dit is ons team programeurs wat voor u deze webshop heeft gemaakt
+				</p>
+				<ul>
+					<li>Owain van Brakel</li>
+					<li>Mies van der Lippe</li>
+					<li>Sek Phubun</li>
+				</ul>
+			</div>
+			<div class="block social-block">
+				<span class='st_sharethis'></span>
+                <span class='st_facebook'></span>
+                <span class='st_amazon_wishlist'></span>
+                <span class='st_delicious'></span>
+                <span class='st_blogger'></span>
+                <span class='st_googleplus'></span>
+                <span class='st_twitter'></span>
+                <span class='st_linkedin'></span>
+                <span class='st_pinterest'></span>
+                <span class='st_email'></span>
+                <script type="text/javascript">var switchTo5x=false;</script>
+				<script type="text/javascript" src="../../../w.sharethis.com/button/buttons.js"></script>
+				<script type="text/javascript">stLight.options({publisher: "20635c27-623e-4e91-8489-7537a83b882b"});</script>
+			</div>
+		</aside>
+		<div class="clearfix"></div>
+		<div class="news-letter-container">
+			<div class="free-shipping-block">
+				<h1>ENJOY FREE SHIPPING</h1>
+				<p>on all orders as our holiday gift for you!</p>
+			</div>
+			<div class="news-letter-block">
+				<h2>SIGN UP FOR OUR NEWSLETTER</h2>
+				<input type="text" value="Enter email address" title="" />
+				<input class="submit-btn" type="submit" value="Submit" title="Submit" />
+			</div>
+		</div>	
+	</div>
+</section>
+</div>
