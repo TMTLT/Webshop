3442144fbc84c0fb114ff3517486148d126abcf3
Owain van Brakel <owain.vanbrakel@gmail.com>
2014-12-04 13:41:02 +0100

    Create settings model and controller

---
+++ application/controllers/settings.php
@@ -0,0 +1,15 @@
+<?php
+
+    /**
+     * Class Settings
+     */
+    class Settings extends MY_Controller {
+
+        /**
+         *
+         */
+        public function __construct() {
+            parent::__construct();
+            $this->load->helper('url');
+        }
+    }
