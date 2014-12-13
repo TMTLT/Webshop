fd8f0e9a01239d4221c25674cd74c0c45b4921c3
Owain van Brakel <owain.vanbrakel@gmail.com>
2014-12-13 04:46:51 +0100

    Remove double function and rename

--- application/views/store/checkout.php
+++ application/views/store/checkout.php
@@ -35,18 +35,7 @@
 
 
 <script type="text/javascript">
-    Number.prototype.formatMoney = function (c, d, t) {
-        var n = this,
-            c = isNaN(c = Math.abs(c)) ? 2 : c,
-            d = d == undefined ? "," : d,
-            t = t == undefined ? "." : t,
-            s = n < 0 ? "-" : "",
-            i = parseInt(n = Math.abs(+n || 0).toFixed(c)) + "",
-            j = (j = i.length) > 3 ? j % 3 : 0;
-        return s + (j ? i.substr(0, j) + t : "") + i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + t) + (c ? d + Math.abs(n - i).toFixed(c).slice(2) : "");
-    };
-
-    function updatecart() {
+    function checkoutinfo() {
         $.ajax({
             url: '<?php echo base_url(); ?>store/cartcontent/',
             success: function (resp) {
@@ -82,5 +71,5 @@
             }
         });
     }
-    updatecart();
+    checkoutinfo();
 </script>
