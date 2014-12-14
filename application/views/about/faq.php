1bfa60765f29c2250f35c4a20b7de4bef1f42e3d
Owain van Brakel <owain.vanbrakel@gmail.com>
2014-12-14 03:14:03 +0100

    Finally remove the free shipping message

--- application/views/about/faq.php
+++ application/views/about/faq.php
@@ -141,18 +141,6 @@
             </div>
         </aside>
         <div class="clearfix"></div>
-        <div class="news-letter-container">
-            <div class="free-shipping-block">
-                <h1>ENJOY FREE SHIPPING</h1>
-
-                <p>on all orders as our holiday gift for you!</p>
-            </div>
-            <div class="news-letter-block">
-                <h2>SIGN UP FOR OUR NEWSLETTER</h2>
-                <input type="text" value="Enter email address" title=""/>
-                <input class="submit-btn" type="submit" value="Submit" title="Submit"/>
-            </div>
-        </div>
     </div>
 </section>
 </div>
--- application/views/store/detail.php
+++ application/views/store/detail.php
@@ -415,18 +415,6 @@
             </div>
         </div>
         <div class="clearfix"></div>
-        <div class="news-letter-container">
-            <div class="free-shipping-block">
-                <h1>ENJOY FREE SHIPPING</h1>
-
-                <p>on all orders as our holiday gift for you!</p>
-            </div>
-            <div class="news-letter-block">
-                <h2>SIGN UP FOR OUR NEWSLETTER</h2>
-                <input type="text" value="Enter email address" title=""/>
-                <input type="submit" value="Submit" title="Submit"/>
-            </div>
-        </div>
     </div>
 </section>
 </div>
